3cb4011f729c7a1cc19dbf6db0fff26b79aebbff
abdullahehab <nrtroz.ae@gmail.com>
2021-12-14 01:12:54 +0200

    complete poeple screen

--- lib/screens/people_screen/people_screen.dart
+++ lib/screens/people_screen/people_screen.dart
@@ -5,6 +5,7 @@ import 'package:admin/screens/main/components/side_menu.dart';
 import 'package:admin/utils/colors.dart';
 import 'package:admin/utils/resources.dart';
 import 'package:admin/widget/main_button.dart';
+import 'package:admin/widget/people_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:admin/extensions/extension.dart';
@@ -36,7 +37,7 @@ class PeopleScreen extends StatelessWidget {
             onPressed: () => context
                 .read<MenuController>()
                 .setSelectedMenuIndex = menuItems.indexOf(menuItems.last),
-            buttonColor: AppColor.kPrimaryDarkColor,
+            buttonColor: AppColor.buttonColor,
           )
         ],
       ),
@@ -75,61 +76,7 @@ class PeopleScreen extends StatelessWidget {
     );
   }
 
-  Container buildSingleCard(BuildContext context) {
-    return Container(
-        width: 300,
-        padding: EdgeInsets.symmetric(horizontal: 2.w),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(3.0)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 1,
-              blurRadius: 3,
-              offset: Offset(0, 1), // changes position of shadow
-            ),
-          ],
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 30,
-                  foregroundColor: Colors.transparent,
-                  child: SvgPicture.asset(
-                    Resources.maleIcon,
-                    width: 40,
-                    height: 40,
-                  ),
-                ),
-                Text("عبد الله ايهاب سعد"),
-                Text("28756475866423"),
-              ],
-            ).addPaddingHorizontalVertical(vertical: 20),
-            Positioned(
-                top: 0,
-                right: 0,
-                child: PopupMenuButton(
-                    padding: EdgeInsets.zero,
-                    icon: Icon(FontAwesomeIcons.ellipsisV),
-                    iconSize: 13,
-                    itemBuilder: (context) => [
-                          PopupMenuItem(
-                            child: Text("تعديل"),
-                            value: 1,
-                          ),
-                          PopupMenuItem(
-                            child: Text("خذف"),
-                            value: 2,
-                          )
-                        ]))
-          ],
-        ));
-  }
+
 
   Container buildSingleMobileCard(BuildContext context) {
     return Container(
