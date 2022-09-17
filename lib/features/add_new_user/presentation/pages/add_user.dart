d9b4b629547f42c7685572fe71c38299cd335375
abdullahehab <nrtroz.ae@gmail.com>
2022-09-17 09:28:50 +0200

    remove husband form

--- lib/features/add_new_user/presentation/pages/add_user.dart
+++ lib/features/add_new_user/presentation/pages/add_user.dart
@@ -1,14 +1,11 @@
 import 'package:admin/extensions/extension.dart';
 import 'package:admin/features/add_new_user/presentation/controller/user_controller.dart';
 import 'package:admin/utils/colors.dart';
-import 'package:admin/utils/text_field_validator.dart';
-import 'package:admin/widget/app_drop_down.dart';
 import 'package:admin/widget/custom_text_field.dart';
-import 'package:admin/widget/date_selector.dart';
 import 'package:admin/widget/gender_selector.dart';
-import 'package:admin/widget/get_date_from_picker_dialog.dart';
 import 'package:admin/widget/main_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
@@ -16,8 +13,6 @@ import '../../../../constants.dart';
 import '../../../../core/shared_components/build_date_time_picker_field.dart';
 import '../../../../core/shared_components/drop_down_widget.dart';
 import '../../../../models/user_model.dart';
-import 'package:get/get.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AddPeople extends GetView<UserController> {
   final _formKey = GlobalKey<FormState>();
@@ -171,132 +166,6 @@ class AddPeople extends GetView<UserController> {
           ),
         ).addPaddingHorizontalVertical(horizontal: 16));
   }
-}
-
-Widget husbandForm(BuildContext context) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text("بيانات الزوج/ الزوجه", style: Theme.of(context).textTheme.headline6)
-          .addPaddingOnly(right: 20),
-      SizedBox(height: 10),
-      CustomTextField(
-        prefixIcon: Icon(FontAwesomeIcons.user, size: APP_ICON_SIZE),
-        contentPadding: EdgeInsets.only(right: 10),
-        // borderColor: AppColor.BORDER_COLOR,
-        onChangedText: (String text) => print('test => $text'),
-        outLineText: 'الاسم',
-        hint: 'الاسم',
-        iconPathWidth: 17,
-        textInputType: TextInputType.text,
-        textInputAction: TextInputAction.next,
-      ),
-      SizedBox(height: 10),
-      CustomTextField(
-        contentPadding: EdgeInsets.only(right: 10),
-        prefixIcon: Icon(FontAwesomeIcons.addressBook, size: APP_ICON_SIZE),
-        // borderColor: AppColor.BORDER_COLOR,
-        onChangedText: (String text) => print('test => $text'),
-        outLineText: 'العنوان',
-        hint: 'العنوان',
-        iconPathWidth: 17,
-        textInputType: TextInputType.text,
-        textInputAction: TextInputAction.next,
-      ),
-      SizedBox(height: 10),
-      CustomTextField(
-        contentPadding: EdgeInsets.only(right: 10),
-        prefixIcon: Icon(FontAwesomeIcons.idCard, size: APP_ICON_SIZE),
-        // borderColor: AppColor.BORDER_COLOR,
-        onChangedText: (String text) => print('test => $text'),
-        outLineText: 'الرقم القومي',
-        hint: 'الرقم القومي',
-        iconPathWidth: 17,
-        textInputType: TextInputType.number,
-        textInputAction: TextInputAction.next,
-      ),
-      SizedBox(height: 10),
-      CustomTextField(
-        contentPadding: EdgeInsets.only(right: 10),
-        prefixIcon: Icon(FontAwesomeIcons.phone, size: APP_ICON_SIZE),
-        // borderColor: AppColor.BORDER_COLOR,
-        onChangedText: (String text) => print('test => $text'),
-        outLineText: 'التليفون',
-        hint: 'التليفون',
-        iconPathWidth: 17,
-        textInputType: TextInputType.phone,
-        textInputAction: TextInputAction.next,
-      ),
-      SizedBox(height: 10),
-      CustomTextField(
-        contentPadding: EdgeInsets.only(right: 10),
-        prefixIcon: Icon(FontAwesomeIcons.phone, size: APP_ICON_SIZE),
-        // borderColor: AppColor.BORDER_COLOR,
-        onChangedText: (String text) => print('test => $text'),
-        outLineText: 'حيازه',
-        hint: 'حيازه',
-        iconPathWidth: 17,
-        textInputType: TextInputType.phone,
-        textInputAction: TextInputAction.next,
-      ),
-      SizedBox(height: 10),
-      Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[Text('تاريخ الميلاد')],
-          ).addPaddingOnly(bottom: 8),
-          InkWell(
-            onTap: () async {
-              // await openPickerDateSingle(context);
-            },
-            child: ValueListenableBuilder<DateTime?>(
-                valueListenable: checkInNotifier,
-                builder: (BuildContext context, DateTime? dateTime, _) {
-                  return DateSelector(
-                    dateTime: dateTime,
-                    enable: false,
-                    onChanged: (_) => print('selected DateTime => ${dateTime}'),
-                  );
-                }),
-          ),
-        ],
-      ),
-      CustomTextField(
-        contentPadding: EdgeInsets.only(right: 10),
-        prefixIcon: Icon(FontAwesomeIcons.phone, size: APP_ICON_SIZE),
-        // borderColor: AppColor.BORDER_COLOR,
-        onChangedText: (String text) => print('test => $text'),
-        outLineText: 'الوظيفه',
-        hint: 'الوظيفه',
-        iconPathWidth: 17,
-        textInputType: TextInputType.text,
-        textInputAction: TextInputAction.next,
-      ),
-      SizedBox(height: 10),
-      ValueListenableBuilder(
-          valueListenable: _selectedHealth,
-          builder: (BuildContext context, String? status, _) {
-            return AppDropdownButton<String>(
-                validator: TextFieldValidators.isNotEmpty,
-                outLineText: 'الحالة الصحية',
-                hintText: 'اختر الحالة الصحية',
-                value: status,
-                items: healthKeys.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onSaved: (vlu) => print('value => $vlu'),
-                onChanged: (dynamic value) {
-                  _selectedHealth.value = value;
-                });
-          }),
-      SizedBox(
-        height: 10,
-      ),
-    ],
-  ).addPaddingOnly(bottom: context.mediaQueryPadding.bottom);
 }
 
 class HorizontalLabeledWidget extends StatelessWidget {
