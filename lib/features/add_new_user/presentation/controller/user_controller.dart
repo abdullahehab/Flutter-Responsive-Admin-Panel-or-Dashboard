672058c755cf6040045f2bb68cdd3c59bcb35ad7
abdullahehab <nrtroz.ae@gmail.com>
2022-10-08 19:19:05 +0200

    user controller

--- lib/features/add_new_user/presentation/controller/user_controller.dart
+++ lib/features/add_new_user/presentation/controller/user_controller.dart
@@ -2,9 +2,24 @@ import 'package:admin/features/add_new_user/domain/usecase/add_user_usecase.dart
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
-class UserController extends GetxController {
-  UserController(this.addUserUsecase);
-  AddUserUsecase addUserUsecase;
+import '../../../../models/user_model.dart';
+import '../../../../widget/components.dart';
+class UserController extends GetxController with StateMixin {
+  UserController(this._addUserUsecase);
+  AddUserUsecase _addUserUsecase;
   var isLoading = false.obs;
 
+
+  addUser(UserModel user) async {
+    var data = await _addUserUsecase.call(user);
+
+    data.fold(
+      (failure) {
+        showToast(message: failure.mess);
+      },
+      (done) {
+        showToast(message: 'تم الاضافه بنجاح');
+      },
+    );
+  }
 }
