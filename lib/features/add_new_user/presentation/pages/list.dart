ce7f6c3b5691e0346ed98df666f7e4849dd049d0
abdullahehab <nrtroz.ae@gmail.com>
2022-10-22 15:39:22 +0200

    add pop up

--- lib/features/add_new_user/presentation/pages/list.dart
+++ lib/features/add_new_user/presentation/pages/list.dart
@@ -1,32 +1,43 @@
+import 'dart:js';
+
 import 'package:admin/features/add_new_user/domain/entities/user_entity.dart';
 import 'package:admin/widget/main_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
-
+import 'package:admin/extensions/extension.dart';
+import '../../../../core/constants/constants.dart';
+import '../../../../core/shared_components/build_date_time_picker_field.dart';
+import '../../../../core/shared_components/build_dialog.dart';
+import '../../../../core/shared_components/drop_down_widget.dart';
 import '../../../../core/shared_components/styled_content_widget.dart';
 import '../../../../core/shared_page/app_empty.dart';
+import '../../../../models/user_model.dart';
+import '../../../../utils/text_field_validator.dart';
+import '../../../../widget/custom_text_field.dart';
 import '../../../../widget/data_cell_item.dart';
 import '../../../../widget/data_column_Item.dart';
 import '../../../../widget/data_controller.dart';
 import '../../../../widget/data_table.dart';
+import '../../../../widget/gender_selector.dart';
 import '../controller/user_controller.dart';
 
 class UsersList extends GetView<UserController> {
   @override
   Widget build(BuildContext context) {
-    return controller.obx((state) => buildBody(state),
+    return controller.obx((state) => buildBody(state, context),
         onLoading: Scaffold(
           body: Center(
             child: CupertinoActivityIndicator(),
           ),
         ),
-        onEmpty: appEmpty(onPressed: () {}));
+        onEmpty: appEmpty(onPressed: () => addEditUserForm(context: context)));
   }
 
-  Scaffold buildBody(List<UserEntity>? users) {
+  Scaffold buildBody(List<UserEntity>? users, BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -38,8 +49,8 @@ class UsersList extends GetView<UserController> {
               leadingWidget: CustomButton(
                   width: 200,
                   height: 50,
-                  text: 'اضافة سكن جديد',
-                  onPressed: () {}),
+                  text: 'اضافة شخص جديد',
+                  onPressed: () => addEditUserForm(context: context)),
               children: [
                 dataTable(
                   columns: [
@@ -68,7 +79,8 @@ class UsersList extends GetView<UserController> {
                         dataCellItem(data: item.housing!),
                         dataCellItem(data: item.healthStatus.toString()),
                         dataController(
-                            onEditPressed: () {},
+                            onEditPressed: () =>
+                                addEditUserForm(model: item, context: context!),
                             onRemovePressed: () {},
                             onViewPressed: () {})
                       ],
@@ -80,6 +92,167 @@ class UsersList extends GetView<UserController> {
           ],
         ),
       ),
+    );
+  }
+
+  void addEditUserForm({UserEntity? model, BuildContext? context}) {
+    final _formKey = GlobalKey<FormState>();
+    bool isEdit = false;
+
+    UserEntity userModel;
+    print('Get.arguments => ${model}');
+    if (model == null) {
+      userModel = UserModel();
+    } else {
+      isEdit = true;
+      userModel = model;
+    }
+
+    showDialog(
+      context: Get.context!,
+      barrierColor: Colors.grey[500]!.withOpacity(.4),
+      useSafeArea: false,
+      builder: (_) => AppDialog(
+          height: 900,
+          title: 'add_new_note'.tr,
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    validator: TextFieldValidators.isName,
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.user, size: APP_ICON_SIZE),
+                    contentPadding: EdgeInsets.only(right: 10),
+                    onChangedText: (String text) => userModel.name = text,
+                    hint: 'الاسم',
+                    outLineText: 'الاسم',
+                    iconPathWidth: 17,
+                    initialValue: userModel.name,
+                    textInputType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    validator: TextFieldValidators.isAddress,
+                    contentPadding: EdgeInsets.only(right: 10),
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.addressBook, size: APP_ICON_SIZE),
+                    onChangedText: (String text) => userModel.address = text,
+                    hint: 'العنوان',
+                    outLineText: 'العنوان',
+                    iconPathWidth: 17,
+                    initialValue: model!.address,
+                    textInputType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    validator: TextFieldValidators.isPhone,
+                    contentPadding: EdgeInsets.only(right: 10),
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.addressBook, size: APP_ICON_SIZE),
+                    onChangedText: (String text) => userModel.phone = text,
+                    hint: 'رقم الهاتف',
+                    outLineText: 'رقم الهاتف',
+                    initialValue: model.phone,
+                    iconPathWidth: 17,
+                    textInputType: TextInputType.phone,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    validator: TextFieldValidators.isNationalId,
+                    contentPadding: EdgeInsets.only(right: 10),
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.idCard, size: APP_ICON_SIZE),
+                    onChangedText: (String text) => userModel.nationalId = text,
+                    hint: 'الرقم القومي',
+                    outLineText: 'الرقم القومي',
+                    initialValue: model.name,
+                    iconPathWidth: 17,
+                    textInputType: TextInputType.number,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    contentPadding: EdgeInsets.only(right: 10),
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.phone, size: APP_ICON_SIZE),
+                    // borderColor: AppColor.BORDER_COLOR,
+                    onChangedText: (String text) => userModel.owning = text,
+                    hint: 'حيازه',
+                    outLineText: 'حيازه',
+                    iconPathWidth: 17,
+                    initialValue: model.owning,
+                    textInputType: TextInputType.phone,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    contentPadding: EdgeInsets.only(right: 10),
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.phone, size: APP_ICON_SIZE),
+                    // borderColor: AppColor.BORDER_COLOR,
+                    onChangedText: (String text) => userModel.housing = text,
+                    hint: 'السكن',
+                    outLineText: 'السكن',
+                    iconPathWidth: 17,
+                    initialValue: model.housing,
+                    textInputType: TextInputType.phone,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  DropDownWidgetX(
+                    labelText: 'الحاله الاجتماعيه',
+                    maxHeight: 100,
+                    items: statusKeys,
+                    selectedItem: statusKeys.first,
+                    onChanged: (value) =>
+                        userModel.socialStatus = value as String,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    validator: TextFieldValidators.isNotEmpty,
+                    contentPadding: EdgeInsets.only(right: 10),
+                    prefixIcon:
+                        Icon(FontAwesomeIcons.phone, size: APP_ICON_SIZE),
+                    onChangedText: (String text) => userModel.working = text,
+                    hint: 'الوظيفه',
+                    outLineText: 'الوظيفه',
+                    initialValue: model.working,
+                    iconPathWidth: 17,
+                    textInputType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(height: 10),
+                  buildDateTimePickerField(
+                    labelText: 'تاريخ الميلاد',
+                    initialValue: model.birthDate,
+                    firstDate: DateTime.now().year,
+                    onSaved: (value) => userModel.birthDate = value,
+                  ),
+                  SizedBox(height: 10),
+                  DropDownWidgetX(
+                    maxHeight: 100,
+                    labelText: 'الحالة الصحية',
+                    items: healthKeys,
+                    selectedItem: healthKeys.first,
+                    onChanged: (value) => userModel.healthStatus = 1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  GenderSelector(
+                    outLineText: 'النوع',
+                    onChanged: (newValue) => userModel.gender = newValue,
+                  ),
+                ],
+              ).addPaddingOnly(bottom: context!.mediaQueryPadding.bottom),
+            ),
+          ).addPaddingHorizontalVertical(horizontal: 16)),
     );
   }
 }
