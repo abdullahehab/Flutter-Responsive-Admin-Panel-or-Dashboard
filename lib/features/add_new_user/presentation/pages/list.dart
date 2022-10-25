16bd2e83ec87bc626e14e6209f296c26b827b0b5
abdullahehab <nrtroz.ae@gmail.com>
2022-10-25 15:23:23 +0200

    StyledContent

--- lib/features/add_new_user/presentation/pages/list.dart
+++ lib/features/add_new_user/presentation/pages/list.dart
@@ -53,115 +53,120 @@ class UsersList extends GetView<UserController> {
         child: ListView(
           children: [
             SizedBox(height: 40.h),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Row(
+            StyledContent(
+              title: "خيارات التصنيف",
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
                     children: [
-                      Expanded(
-                        child: CustomTextField(
-                          validator: TextFieldValidators.isName,
-                          prefixIcon:
+                      Row(
+                        children: [
+                          Expanded(
+                            child: CustomTextField(
+                              validator: TextFieldValidators.isName,
+                              prefixIcon:
                               Icon(FontAwesomeIcons.user, size: APP_ICON_SIZE),
-                          contentPadding: EdgeInsets.only(right: 10),
-                          // onChangedText: (String text) => userModel.name = text,
-                          // initialValue: userModel.name,
-                          hint: 'الاسم',
-                          outLineText: 'الاسم',
-                          iconPathWidth: 17,
-                          textInputType: TextInputType.text,
-                          textInputAction: TextInputAction.next,
-                        ),
+                              contentPadding: EdgeInsets.only(right: 10),
+                              // onChangedText: (String text) => userModel.name = text,
+                              // initialValue: userModel.name,
+                              hint: 'الاسم',
+                              outLineText: 'الاسم',
+                              iconPathWidth: 17,
+                              textInputType: TextInputType.text,
+                              textInputAction: TextInputAction.next,
+                            ),
+                          ),
+                          SizedBox(width: 2.w),
+                          Expanded(
+                            child: CustomTextField(
+                              validator: TextFieldValidators.isPhone,
+                              contentPadding: EdgeInsets.only(right: 10),
+                              prefixIcon: Icon(FontAwesomeIcons.addressBook,
+                                  size: APP_ICON_SIZE),
+                              // onChangedText: (String text) => userModel.phone = text,
+                              // initialValue: userModel.phone,
+                              hint: 'الرقم القومي',
+                              outLineText: 'الرقم القومي',
+                              iconPathWidth: 17,
+                              maxLength: 11,
+                              textInputType: TextInputType.phone,
+                              textInputAction: TextInputAction.next,
+                            ),
+                          ),
+                          SizedBox(width: 2.w),
+                          Expanded(
+                            child: DropDownWidgetX<Work>(
+                              requiredFiled: true,
+                              labelText: 'الوظيفة',
+                              // selectedItem: workController.getById(userModel.working!),
+                              itemAsString: (Work? u) => u!.title!,
+                              maxHeight: 100,
+                              items: workController.workList,
+                              onChanged: (value) {
+                                var selected = value as Work;
+                                // userModel.working = selected.id.toString();
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 2.w),
+                        ],
                       ),
-                      SizedBox(width: 2.w),
-                      Expanded(
-                        child: CustomTextField(
-                          validator: TextFieldValidators.isPhone,
-                          contentPadding: EdgeInsets.only(right: 10),
-                          prefixIcon: Icon(FontAwesomeIcons.addressBook,
-                              size: APP_ICON_SIZE),
-                          // onChangedText: (String text) => userModel.phone = text,
-                          // initialValue: userModel.phone,
-                          hint: 'الرقم القومي',
-                          outLineText: 'الرقم القومي',
-                          iconPathWidth: 17,
-                          maxLength: 11,
-                          textInputType: TextInputType.phone,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      SizedBox(width: 2.w),
-                      Expanded(
-                        child: DropDownWidgetX<Work>(
-                          requiredFiled: true,
-                          labelText: 'الوظيفة',
-                          // selectedItem: workController.getById(userModel.working!),
-                          itemAsString: (Work? u) => u!.title!,
-                          maxHeight: 100,
-                          items: workController.workList,
-                          onChanged: (value) {
-                            var selected = value as Work;
-                            // userModel.working = selected.id.toString();
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 2.w),
+                      SizedBox(height: 10.h),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: DropDownWidgetX<Owning>(
+                              requiredFiled: true,
+                              labelText: 'الحيازة',
+                              // selectedItem: owningController.getById(userModel.owning!),
+                              itemAsString: (Owning? u) => u!.title!,
+                              maxHeight: 100,
+                              items: owningController.owningList,
+                              onChanged: (value) {
+                                var selected = value as Owning;
+                                // userModel.owning = selected.id.toString();
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 2.w),
+                          Expanded(
+                              child: DropDownWidgetX<Housing>(
+                                requiredFiled: true,
+                                labelText: 'السكن',
+                                // selectedItem: housingController.getById(userModel.housing!),
+                                itemAsString: (Housing? u) => u!.title!,
+                                maxHeight: 100,
+                                items: housingController.housingList,
+                                onChanged: (value) {
+                                  var selected = value as Housing;
+                                  // userModel.housing = selected.id.toString();
+                                },
+                              )),
+                          SizedBox(width: 2.w),
+                          Expanded(
+                            child: DropDownWidgetX<SocialStatus>(
+                              requiredFiled: true,
+                              labelText: 'الحالة الاجتماعية',
+                              // selectedItem:
+                              // socialStatusController.getById(userModel.socialStatus!),
+                              itemAsString: (SocialStatus? u) => u!.title!,
+                              maxHeight: 100,
+                              items: socialStatusController.socialStatusList,
+                              onChanged: (value) {
+                                var selected = value as SocialStatus;
+                                // userModel.socialStatus = selected.id.toString();
+                              },
+                            ),
+                          ),
+
+                          SizedBox(width: 2.w),
+                        ],
+                      )
                     ],
                   ),
-                  SizedBox(height: 10.h),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: DropDownWidgetX<Owning>(
-                          requiredFiled: true,
-                          labelText: 'الحيازة',
-                          // selectedItem: owningController.getById(userModel.owning!),
-                          itemAsString: (Owning? u) => u!.title!,
-                          maxHeight: 100,
-                          items: owningController.owningList,
-                          onChanged: (value) {
-                            var selected = value as Owning;
-                            // userModel.owning = selected.id.toString();
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 2.w),
-                      Expanded(
-                          child: DropDownWidgetX<Housing>(
-                        requiredFiled: true,
-                        labelText: 'السكن',
-                        // selectedItem: housingController.getById(userModel.housing!),
-                        itemAsString: (Housing? u) => u!.title!,
-                        maxHeight: 100,
-                        items: housingController.housingList,
-                        onChanged: (value) {
-                          var selected = value as Housing;
-                          // userModel.housing = selected.id.toString();
-                        },
-                      )),
-                      SizedBox(width: 2.w),
-                      Expanded(
-                        child: DropDownWidgetX<SocialStatus>(
-                          requiredFiled: true,
-                          labelText: 'الحالة الاجتماعية',
-                          // selectedItem:
-                          // socialStatusController.getById(userModel.socialStatus!),
-                          itemAsString: (SocialStatus? u) => u!.title!,
-                          maxHeight: 100,
-                          items: socialStatusController.socialStatusList,
-                          onChanged: (value) {
-                            var selected = value as SocialStatus;
-                            // userModel.socialStatus = selected.id.toString();
-                          },
-                        ),
-                      ),
-
-                      SizedBox(width: 2.w),
-                    ],
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
             SizedBox(height: 40.h),
             StyledContent(
